ff21fc8045103c25a2ca49724781c350480e2c34
spamabd <143559506+spamabd@users.noreply.github.com>
2024-03-03 12:02:28 -0800

    Create ex_10.ts

---
+++ Assignment-10/ex_10.ts
@@ -0,0 +1,7 @@
+//Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+const num1: number = 24;
+const num2: number = 29;
+
+// this will add the num1 and num2 (this is is comment in typescript)
+//console.log(num1 + num2);
