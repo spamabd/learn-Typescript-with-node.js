dd134a2a75098babab2a0e87fbc54045b99bbfa4
spamabd <143559506+spamabd@users.noreply.github.com>
2024-03-03 10:52:16 -0800

    Create ex_6.ts

---
+++ Assignment-6/ex_6.ts
@@ -0,0 +1,8 @@
+//Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+const myNamee: string ="\tAbdul Raheem";
+// \t used for tab/space
+const myName: string ="\nAbdul Raheem";
+// \n used for new line
+console.log(myNamee);
+console.log(myName);
