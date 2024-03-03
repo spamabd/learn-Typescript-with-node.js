3a4682881ec8022b589bbc13e6756ceaadbc637d
spamabd <143559506+spamabd@users.noreply.github.com>
2024-03-03 11:14:05 -0800

    Create ex_7.js

---
+++ Assignment-7/ex_7.js
@@ -0,0 +1,6 @@
+// Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+var addition = 65 + 4;
+var subtraction = 70 - 1;
+var multiplication = 3 * 23;
+var division = 138 / 2;
+console.log(addition, subtraction, multiplication, division);
