eaa58e91dbd6a05acff2748e7fefe8f5d9fc96d2
spamabd <143559506+spamabd@users.noreply.github.com>
2024-03-03 11:45:24 -0800

    Create ex_9.js

---
+++ Assignment-9/ex_9.js
@@ -0,0 +1,4 @@
+//Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favNum = 7;
+var message = "My favorite Number is \"".concat(favNum, "\"");
+console.log(message);
