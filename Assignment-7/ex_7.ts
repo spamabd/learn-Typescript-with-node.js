7eeb031aa4e5f563d91d7e6d35b14cebbe022937
spamabd <143559506+spamabd@users.noreply.github.com>
2024-03-03 11:13:55 -0800

    Create ex_7.ts

---
+++ Assignment-7/ex_7.ts
@@ -0,0 +1,8 @@
+// Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+const addition: number = 65 + 4;
+const subtraction: number = 70 - 1;
+const multiplication: number = 3 * 23;
+const division: number = 138 / 2;
+
+console.log(addition,subtraction,multiplication,division);
