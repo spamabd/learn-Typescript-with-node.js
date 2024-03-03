8398958621db7aa094fb686aa5e884b191700bf9
spamabd <143559506+spamabd@users.noreply.github.com>
2024-03-03 11:43:08 -0800

    Create ex_9.ts

---
+++ Assignment-9/ex_9.ts
@@ -0,0 +1,5 @@
+//Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+const favNum: number = 7;
+const message: string = `My favorite Number is ${favNum}`;
+console.log(message);
