a759716b8ce18bfb6565d36d4f6bdee70b4ce840
spamabd <143559506+spamabd@users.noreply.github.com>
2024-03-05 15:17:50 -0800

    Create ex-11.ts

---
+++ ex-11.ts
@@ -0,0 +1,8 @@
+//Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let freindsName: string[] =["Ali" , "Hussain" , "yasir", "Habib"];
+
+console.log(freindsName[0]);
+console.log(freindsName[1]);
+console.log(freindsName[2]);
+console.log(freindsName[3]);
