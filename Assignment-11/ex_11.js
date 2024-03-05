a80a3b654ee20d110adee6854598bc637d15b531
spamabd <143559506+spamabd@users.noreply.github.com>
2024-03-05 15:16:38 -0800

    Create ex_11.js

---
+++ Assignment-11/ex_11.js
@@ -0,0 +1,6 @@
+//Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+var freindsNasme = ["Ali", "Hussain", "yasir", "Habib"];
+console.log(freindsNasme[0]);
+console.log(freindsNasme[1]);
+console.log(freindsNasme[2]);
+console.log(freindsNasme[3]);
