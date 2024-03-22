62b1d36d0155286b7ef42980fc419acaf6025f67
spamabd <143559506+spamabd@users.noreply.github.com>
2024-03-22 05:37:42 -0700

    Create ex_12.ts

---
+++ Assignment-12/ex_12.ts
@@ -0,0 +1,8 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let freindsName: string[] =["Ali" , "Hussain" , "yasir", "Habib"];
+
+console.log(`hello ${freindsName[0]}, how are you?`);
+console.log(`hello ${freindsName[3]}, how are you?`);
+console.log(`hello ${freindsName[1]}, how are you?`);
+console.log(`hello ${freindsName[2]}, how are you?`);
