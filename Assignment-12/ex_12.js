1e6ef45df84c01a8f053384fcb94c0c232522105
spamabd <143559506+spamabd@users.noreply.github.com>
2024-03-22 05:38:27 -0700

    Create ex_12.js

---
+++ Assignment-12/ex_12.js
@@ -0,0 +1,6 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+var freindsName = ["Ali", "Hussain", "yasir", "Habib"];
+console.log("hello ".concat(freindsName[0], ", how are you?"));
+console.log("hello ".concat(freindsName[3], ", how are you?"));
+console.log("hello ".concat(freindsName[1], ", how are you?"));
+console.log("hello ".concat(freindsName[2], ", how are you?"));
